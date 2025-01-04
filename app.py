be54b900692d604afd76b62f80f2abfe1dde7e19
TWQ <rr4143130@gmail.com>
2025-01-05 00:11:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, static_folder='')  # بدون مجلد static
+app = Flask(__name__)  # بدون static_folder
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # تأكد أن index.html في مجلد templates
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # الحصول على المنفذ من المتغيرات البيئية
+    app.run(host="0.0.0.0", port=port)  # 
