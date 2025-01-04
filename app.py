ccaebdb1a7049dd65d581ad70b488ccd58e46fca
TWQ <rr4143130@gmail.com>
2025-01-04 17:01:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')  # عرض ملف index.html
+    return "Hello, Heroku!"
 
-if name == "main":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # استخدم منفذ Heroku أو افتراضيًا 5000
+    app.run(host='0.0.0.0', port=port)
