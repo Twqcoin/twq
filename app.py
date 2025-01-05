e32fc13c387f4e66fe3b45d09498e0c0b1ab7277
TWQ <rr4143130@gmail.com>
2025-01-05 17:24:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'HEAD'])
 def home():
-    return render_template('index.html')  # قم بإنشاء هذا الملف داخل مجلد templates
+    return render_template('index.html')  # تأكد من أن الملف موجود في مجلد templates
+
 
 if __name__ == "__main__":
     app.run(debug=True)
