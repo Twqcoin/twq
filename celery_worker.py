0353108ac3e4f8a31201683f24710741e30a7fcf
TWQ <rr4143130@gmail.com>
2025-01-12 18:45:02 +0300

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,10 +1,14 @@
 from celery import Celery
 
 def make_celery(app):
+    # تهيئة كائن Celery
     celery = Celery(
         app.import_name,
         backend=app.config['CELERY_RESULT_BACKEND'],
         broker=app.config['CELERY_BROKER_URL']
     )
+    
+    # تحديث إعدادات Celery باستخدام إعدادات Flask
     celery.conf.update(app.config)
+    
     return celery
