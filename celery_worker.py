70657deebd66f4f47c1280ea2876c2135c4d18a3
TWQ <rr4143130@gmail.com>
2025-01-12 23:54:42 +0300

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,15 +1,10 @@
 from celery import Celery
 
 def make_celery(app):
-    # إنشاء كائن Celery مع إعدادات التطبيق
     celery = Celery(
         app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],  # مكان تخزين النتائج
-        broker=app.config['CELERY_BROKER_URL']  # مكان تخزين الرسائل (الـ Queue)
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
     )
-
-    # تحديث إعدادات Celery باستخدام إعدادات Flask
     celery.conf.update(app.config)
-
-    # إرجاع كائن Celery المعدل
     return celery
