e3591f9f2123b3f3c73b7ea141171386dd2116fc
TWQ <rr4143130@gmail.com>
2025-01-12 14:29:12 +0300

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,11 +1,10 @@
 from celery import Celery
 
 def make_celery(app):
-    # إعداد Celery مع Flask
     celery = Celery(
         app.import_name,
         backend=app.config['CELERY_RESULT_BACKEND'],
         broker=app.config['CELERY_BROKER_URL']
     )
-    celery.conf.update(app.config)  # تطبيق إعدادات Flask على Celery
+    celery.conf.update(app.config)
     return celery
