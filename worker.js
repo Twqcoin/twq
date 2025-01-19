79b3c96c94bb73f8b1b6fc2a9209846fda698c10
TWQ <rr4143130@gmail.com>
2025-01-19 16:21:37 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,19 +1,17 @@
 self.onmessage = function(event) {
     if (event.data === 'startMining') {
         console.log('Mining started...');
-        // نفذ عملية التعدين هنا
-        // على سبيل المثال، عملية محاكاة للتعدين
+        // محاكاة لعملية التعدين (يمكنك استبدالها بكود تعدين حقيقي)
         let progress = 0;
         const miningInterval = setInterval(() => {
             progress += 10;
             console.log(`Mining progress: ${progress}%`);
-            postMessage(progress);  // إرسال التقدم إلى الكونسول
-            
+            postMessage(progress);  // إرسال التقدم إلى الصفحة الرئيسية
             if (progress >= 100) {
                 clearInterval(miningInterval);
                 console.log('Mining complete');
                 postMessage('MiningComplete');  // إرسال رسالة اكتمال التعدين
             }
-        }, 1000);  // قم بتكرار كل ثانية لمحاكاة التقدم
+        }, 1000);  // نفذ كل ثانية لمحاكاة التقدم
     }
 };
