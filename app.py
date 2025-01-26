d8fabad66eb282b7a42e44b602e5872f3879d353
TWQ <rr4143130@gmail.com>
2025-01-26 15:26:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app import routes  # استيراد نقاط النهاية من routes.py
+from routes import bp  # استيراد نقاط النهاية من routes.py
 
 # إنشاء تطبيق Flask
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object('config.Config')
 
 # تسجيل نقاط النهاية (Routes)
-app.register_blueprint(routes.bp)  # إذا كنت تستخدم Blueprint
+app.register_blueprint(bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
