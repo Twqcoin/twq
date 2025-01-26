1fdbea3467a53b9d6231a878329a55ad2d2dc729
TWQ <rr4143130@gmail.com>
2025-01-26 15:15:23 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-# إنشاء نسخة Flask
+# إنشاء تطبيق Flask
 app = Flask(__name__)
 
 # تهيئة الإعدادات
 app.config.from_object('config.Config')
 
-# تسجيل نقاط النهاية (Routes)
+# استيراد نقاط النهاية (Routes)
 from app import routes
