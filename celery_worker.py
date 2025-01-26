494665bc71fbf40093f7e81b078d5c389c38cef5
TWQ <rr4143130@gmail.com>
2025-01-26 15:28:00 +0300

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,4 +1,13 @@
-from app import celery
+from celery import Celery
+from app import app  # استيراد تطبيق Flask من app.py
 
-if __name__ == "__main__":
-    celery.worker_main(['worker', '--loglevel=info'])
+# تهيئة Celery
+celery = Celery(
+    app.import_name,
+    broker=app.config['CELERY_BROKER_URL'],
+    backend=app.config['CELERY_RESULT_BACKEND']
+)
+celery.conf.update(app.config)
+
+if __name__ == '__main__':
+    celery.start()
