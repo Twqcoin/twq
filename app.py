a1c687d721e5ded47cdb94c5094def920ffd87df
TWQ <rr4143130@gmail.com>
2025-02-09 22:41:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def serve_static(filename):
 # تشغيل الخادم
 if __name__ == '__main__':
     create_db()
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
+
